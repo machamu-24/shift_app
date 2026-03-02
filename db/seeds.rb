04213ae561f0c011372b8e2ad308eb6758011c81
machamu-24 <masamu420@gmail.com>
2026-03-02 20:53:17 +0900

    Force overwrite admin password on seed to fix login issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,13 +21,13 @@ names = %w[
   清水
 ]
 
-Staff.find_or_create_by!(email: "admin@example.com") do |staff|
-  staff.name = "管理者"
-  staff.password = "password"
-  staff.password_confirmation = "password"
-  staff.role = :admin
-  staff.is_leader = true
-end
+admin = Staff.find_or_initialize_by(email: "admin@example.com")
+admin.name = "管理者"
+admin.password = "password"
+admin.password_confirmation = "password"
+admin.role = :admin
+admin.is_leader = true
+admin.save!
 
 names.each_with_index do |name, index|
   Staff.find_or_create_by!(email: "staff#{index + 1}@example.com") do |staff|
